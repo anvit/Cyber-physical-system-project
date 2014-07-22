4e86819143b9e7707085f1df78858442545782f4
Priya Ahuja <priya18.ahuja@gmail.com>
2014-07-22 14:16:07 -0500

    Added Source and destination IP counters for the IP 172.16.2.34

--- bro_log_reader.py
+++ bro_log_reader.py
@@ -12,6 +12,9 @@ duration = []
 src_dst = []
 src_dst_split = []
 count_each = []
+select_src_ip = []
+select_dst_ip = []
+
 for j in range(0,166):
 	addr = '../Large-Logs/pcap'+str(j)+'/conn.log'
 	num_lines = sum(1 for line in open(addr))
@@ -31,12 +34,18 @@ for j in range(0,166):
 				duration.append(row[8])
 				src_dst.append(row[2]+":"+row[4])
 				src_dst_split.append(row[2]+":"+row[4])
+				if(row[2]=='172.16.2.34'):
+					select_src_ip.append(row[4])
+				elif(row[4]=='172.16.2.34'):
+					select_dst_ip.append(row[2])
 	if j%14 == 0 :
-		print j
 		count_each.append(Counter(src_dst_split))
 		del src_dst_split[:]
 
 count_freq = Counter(src_dst)
+
+count_src_freq = Counter(select_src_ip)
+count_dst_freq = Counter(select_dst_ip)
 
 ip_set = list(set(ip))
 con = np.empty((len(ip_set), len(ip_set)), dtype=object)
@@ -61,7 +70,13 @@ ip_list_file.write(str(sorted(ip_set)))
 connect_freq_file = open('../count_freq.txt', 'w')
 connect_freq_file.write(str(count_freq))
 
-print len(count_each)
+src_freq_file = open('../count_src_freq.txt', 'w')
+src_freq_file.write(str(count_src_freq))
+
+dst_freq_file = open('../count_dst_freq.txt', 'w')
+dst_freq_file.write(str(count_dst_freq))
+
+
 for i in range(0,len(count_each)):
 	connect_freq_file = open('../count_freq_split'+str(i)+'.txt', 'w')
 	connect_freq_file.write(str(count_each[i]))
