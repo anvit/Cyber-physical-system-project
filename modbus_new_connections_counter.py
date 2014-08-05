066e3e1d9a7b4e7d5ba287a9eb65467b89fa5d5e
Priya Ahuja <priya18.ahuja@gmail.com>
2014-08-05 12:13:29 -0500

    Fixed minor bug in modbus new ips counter

--- modbus_new_connections_counter.py
+++ modbus_new_connections_counter.py
@@ -5,11 +5,7 @@ import numpy as np
 
 src_ip = []
 dst_ip = []
-func = []
 src_dst = []
-select_src_ip = []
-select_dst_ip = []
-diff_host = []
 time = []
 ctr = 0
 for j in range(0,166):
@@ -25,59 +21,58 @@ for j in range(0,166):
 				src_ip.append(row[2])
 				time.append(float(row[0]))
 				dst_ip.append(row[4])
-				func.append(row[6])
+				src_dst.append(row[2]+"*" + row[4])
 				ctr = ctr + 1
-
-# conns_per_sec = []
-# conn = 0
-# r0 = time[0]
-# ip_set_chk = []
-
-# for i in range(0,len(dst_ip)):
-# 	print i
-# 	if(time[i]>=r0 and time[i]<(r0+1)):
-# 		if (dst_ip[i] in ip_set_chk) == False:
-# 			ip_set_chk.append(dst_ip[i])
-# 			conn = conn + 1
-# 	elif(time[i]>=(r0+1)):
-# 		r0 = r0 + 1
-# 		if (dst_ip[i] in ip_set_chk) == False:
-# 			ip_set_chk.append(dst_ip[i])
-# 			conn = conn + 1
-# 		conns_per_sec.append(conn)
-# 		conn = 0
 
 conns_per_min = []
 conn = 0
 r0 = time[0]
 ip_n_set_chk = []
-for i in range(0,len(dst_ip)):
+for i in range(0,len(src_dst)):
 	print i
 	if(time[i]>=r0 and time[i]<(r0+60)):
-		if (dst_ip[i] in ip_n_set_chk) == False:
-			ip_n_set_chk.append(dst_ip[i])
-			conn = conn + 1
-		elif (src_ip[i] in ip_n_set_chk) == False:
-			ip_n_set_chk.append(src_ip[i])
+		if (src_dst[i] in ip_n_set_chk) == False:
+			ip_n_set_chk.append(src_dst[i])
 			conn = conn + 1
 	elif(time[i]>=(r0+60)):
 		r0 = r0 + 60
-		if (dst_ip[i] in ip_n_set_chk) == False:
-			ip_n_set_chk.append(dst_ip[i])
-			conn = conn + 1
-		elif (src_ip[i] in ip_n_set_chk) == False:
-			ip_n_set_chk.append(src_ip[i])
+		if (src_dst[i] in ip_n_set_chk) == False:
+			ip_n_set_chk.append(src_dst[i])
 			conn = conn + 1
 		conns_per_min.append(conn)
 		conn = 0
 
-
-# cps_file = open('../modbus_cps.csv', 'w')
-# for i in range(0,len(conns_per_sec)):
-# 	cps_file.write(str(i) + "," + str(conns_per_sec[i]) + "\n")
-# cps_file.close()
-
-cpm_file = open('../modbus_cpm.csv', 'w')
+cpm_file = open('../modbus_conn_cpm.csv', 'w')
 for i in range(0,len(conns_per_min)):
 	cpm_file.write(str(i) + "," + str(conns_per_min[i]) + "\n")
+cpm_file.close()
+
+
+conns_ip_per_min = []
+conn_ip = 0
+r0 = time[0]
+ip_set_chk = []
+for i in range(0,len(dst_ip)):
+	print i
+	if(time[i]>=r0 and time[i]<(r0+60)):
+		if (dst_ip[i] in ip_set_chk) == False:
+			ip_set_chk.append(dst_ip[i])
+			conn_ip = conn_ip + 1
+		elif (src_ip[i] in ip_set_chk) == False:
+			ip_set_chk.append(src_ip[i])
+			conn_ip = conn_ip + 1
+	elif(time[i]>=(r0+60)):
+		r0 = r0 + 60
+		if (dst_ip[i] in ip_set_chk) == False:
+			ip_set_chk.append(dst_ip[i])
+			conn_ip = conn_ip + 1
+		elif (src_ip[i] in ip_set_chk) == False:
+			ip_set_chk.append(src_ip[i])
+			conn_ip = conn_ip + 1
+		conns_ip_per_min.append(conn_ip)
+		conn_ip = 0
+
+cpm_file = open('../modbus_ip_cpm.csv', 'w')
+for i in range(0,len(conns_ip_per_min)):
+	cpm_file.write(str(i) + "," + str(conns_ip_per_min[i]) + "\n")
 cpm_file.close()
